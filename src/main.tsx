d46143818e45e97a9c289c8ff7dd215359f05cc4
Sean <sxfmao@163.com>
2024-01-30 18:53:13 +0800

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import scss from './index.module.scss'
 import './index.scss'
 
 console.log('chrome', chrome)
+import process from 'process'
+window.process = process
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN} theme={{ cssVar: true, hashed: false }}>
