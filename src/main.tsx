c7ad6859aeeadcd45e0680dfbaa5fa5590372ed5
Sean <sxfmao@163.com>
2024-02-21 17:04:48 +0800

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RecoilRoot } from 'recoil'
 import { ConfigProvider, App } from 'antd'
 import Children from './App'
 import zhCN from 'antd/locale/zh_CN'
@@ -11,10 +12,12 @@ console.log('chrome', chrome)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider locale={zhCN} theme={{ cssVar: true, hashed: false }}>
-      <App className={scss.app}>
-        <Children />
-      </App>
-    </ConfigProvider>
+    <RecoilRoot>
+      <ConfigProvider locale={zhCN} theme={{ cssVar: true, hashed: false }}>
+        <App className={scss.app}>
+          <Children />
+        </App>
+      </ConfigProvider>
+    </RecoilRoot>
   </React.StrictMode>
 )
---
+++ src/store/index.ts
@@ -0,0 +1,6 @@
+import { atom } from 'recoil'
+
+export const demoState = atom({
+  key: 'demo',
+  default: ''
+})
