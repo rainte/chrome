dc41196dc096354f1d60e5588a688d1e2ef6a699
Sean <sxfmao@163.com>
2025-03-21 22:31:37 +0800

    commit

---
+++ .content/content.js
@@ -0,0 +1,1 @@
+"use strict";console.log("content");const t=document.createElement("script");t.setAttribute("type","text/javascript");t.src=chrome.runtime.getURL("insert.js");document.body.appendChild(t);
